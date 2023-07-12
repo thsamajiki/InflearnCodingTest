c544244c3a3b7dc790ff3cef5644a5a10982212e
thsamajiki <ant1102@naver.com>
2023-07-13 01:25:48 +0900

    1. 문자 찾기 풀이

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public int solution(int n, int m) {
-        return 0;
+    public int solution(String str, char t) {
+        int answer = 0;
+
+        str = str.toLowerCase();
+        t = Character.toLowerCase(t);
+        for(int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == t) {
+                answer++;
+            }
+        }
+
+        return answer;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Main main = new Main();
-        int answer = main.solution(0, 0);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        char t = br.readLine().charAt(0);
+
+        System.out.println(main.solution(str, t));
     }
 }
