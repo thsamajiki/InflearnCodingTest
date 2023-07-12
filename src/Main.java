c4973355e78ff1633fb198b989f5434ebb8b367b
thsamajiki <ant1102@naver.com>
2023-07-12 18:56:53 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public int solution(int n, int m) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        int answer = main.solution(0, 0);
+    }
+}
