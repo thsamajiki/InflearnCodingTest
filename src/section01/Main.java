263bfaf142b0a1753f1360ae7409f670500a7feb
thsamajiki <ant1102@naver.com>
2023-07-13 02:13:20 +0900

    2. 대소문자 변환 첫번째 풀이

--- src/section01/Main.java
+++ src/section01/Main.java
@@ -3,18 +3,20 @@ package section01;
 import java.io.*;
 
 public class Main {
-    public int solution(String str, char t) {
-        int answer = 0;
+    public String solution(String str) {
+        StringBuilder sb = new StringBuilder();
 
-        str = str.toLowerCase();
-        t = Character.toLowerCase(t);
         for(char ch: str.toCharArray()) {
-            if (ch == t) {
-                answer++;
+            if (Character.isLowerCase(ch)) {
+                ch = Character.toUpperCase(ch);
+                sb.append(ch);
+            } else {
+                ch = Character.toLowerCase(ch);
+                sb.append(ch);
             }
         }
 
-        return answer;
+        return sb.toString();
     }
 
     public static void main(String[] args) throws IOException {
@@ -22,8 +24,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
-        char t = br.readLine().charAt(0);
 
-        System.out.println(main.solution(str, t));
+        System.out.println(main.solution(str));
     }
 }
