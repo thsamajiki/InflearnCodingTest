050e72ffa65c45cee6cc2895c87265ee6f345910
thsamajiki <ant1102@naver.com>
2023-07-14 21:40:29 +0900

    4. 단어 뒤집기 나의 풀이

--- test/section01/MainTest.java
+++ test/section01/MainTest.java
@@ -3,6 +3,9 @@ package section01;
 import org.junit.jupiter.api.Test;
 import section01.Main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -10,7 +13,7 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals("study", main.solution("it is time to study"));
+        assertLinesMatch(Arrays.asList("doog", "emiT", "giB"), main.solution(3, new String[]{ "good", "Time", "Big" }));
     }
 
     @Test
