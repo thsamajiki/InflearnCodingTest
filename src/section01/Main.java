d566bce4952568fdefac2764d3157484138706b7
thsamajiki <ant1102@naver.com>
2023-07-15 01:44:42 +0900

    7. 회문 문자열 나의 풀이

--- src/section01/Main.java
+++ src/section01/Main.java
@@ -9,11 +9,10 @@ public class Main {
         char[] charArray = str.toCharArray();
         int left = 0;
         int right = str.length() - 1;
-
         String answer = "";
         loop : while(left < right) {
             for(int i = 0; i < charArray.length / 2; i++) {
-                if (str.charAt(i) == str.charAt(right - i)) {
+                if (str.charAt(i) == str.charAt(charArray.length - 1 - i)) {
                     left++;
                     right--;
                     answer = "YES";
@@ -23,16 +22,12 @@ public class Main {
                 }
             }
         }
-
         return answer;
     }
-
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String str = br.readLine();
-
         System.out.println(main.solution(str));
     }
 }
