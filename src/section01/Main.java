176d733451d05a1bf20e62f5ddef0acdaa156d73
thsamajiki <ant1102@naver.com>
2023-07-15 01:48:38 +0900

    7. 회문 문자열 두번째 풀이

--- src/section01/Main.java
+++ src/section01/Main.java
@@ -5,29 +5,25 @@ import java.util.*;
 
 public class Main {
     public String solution(String str) {
+        String answer = "YES";
         str = str.toLowerCase();
-        char[] charArray = str.toCharArray();
-        int left = 0;
-        int right = str.length() - 1;
-        String answer = "";
-        loop : while(left < right) {
-            for(int i = 0; i < charArray.length / 2; i++) {
-                if (str.charAt(i) == str.charAt(charArray.length - 1 - i)) {
-                    left++;
-                    right--;
-                    answer = "YES";
-                } else {
-                    answer = "NO";
-                    break loop;
-                }
+        int len = str.length();
+
+        for(int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - 1 - i)) {
+                return "NO";
             }
         }
+
         return answer;
     }
+
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String str = br.readLine();
+
         System.out.println(main.solution(str));
     }
 }
