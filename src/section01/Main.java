e44824bc71baeb9c29cc66f502c63e7f059cadb0
thsamajiki <ant1102@naver.com>
2023-07-15 02:24:32 +0900

    9. 숫자만 추출 두번째 풀이

--- src/section01/Main.java
+++ src/section01/Main.java
@@ -5,15 +5,13 @@ import java.util.*;
 
 public class Main {
     public int solution(String s) {
-        StringBuilder numStr = new StringBuilder();
+        int answer = 0; // 0으로 초기화
 
         for (char ch : s.toCharArray()) {
-            if (ch - '0' >= 0 && ch - '0' < 10) {
-                numStr.append(ch);
+            if (ch >= 48 && ch <= 57) { // 0(=> 48)부터 9(=> 57)까지의 아스키코드를 이용한 풀이
+                answer = answer * 10 + (ch - 48);
             }
         }
-
-        int answer = Integer.parseInt(numStr.toString());
 
         return answer;
     }
