5227ab780b0eb6b6d684e1d7bfa8fb5b7e897468
thsamajiki <ant1102@naver.com>
2023-07-17 19:23:57 +0900

    6. 뒤집은 소수 인프런 풀이

--- src/section02/Main.java
+++ src/section02/Main.java
@@ -4,16 +4,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public int solution(int n) {
-        int answer = 0;
+    public boolean isPrime(int num) {
+        if (num == 1) {
+            return false;
+        }
 
-        int[] array = new int[n + 1];
-        for (int i = 2; i <= n; i++) {
-            if (array[i] == 0) {
-                answer++;
-                for (int j = i; j <= n; j += i) {
-                    array[j] = 1;
-                }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int temp = arr[i];
+            int res = 0;
+            while (temp > 0) {
+                int t = temp % 10;  // 일의 자리숫자를 찾아냄
+                res = res * 10 + t; // 일의 자리숫자를 새로운 변수의 일의 자리수로 옮김
+                temp /= 10; // 기존의 일의 자리숫자가 사라짐
+            }
+
+            if (isPrime(res)) {
+                answer.add(res);
             }
         }
 
@@ -25,7 +43,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
 
-        System.out.println(main.solution(n));
+        for (int num : main.solution(n, array)) {
+            System.out.print(num + " ");
+        }
     }
 }
