da147c818795823feb718cfa34964fd79dfed577
thsamajiki <ant1102@naver.com>
2023-07-18 03:19:47 +0900

    9. 격자판 최대합 나의 풀이

--- src/section02/Main.java
+++ src/section02/Main.java
@@ -4,19 +4,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public int[] solution(int n, int[] arr) {
-        int[] answer = new int[n];
+    public int solution(int n, int[][] arr) {
+        int temp = 0;
+        int maxRow = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                temp += arr[i][j];
+            }
+            maxRow = Math.max(maxRow, temp);
+            temp = 0;
+        }
+
+        int maxCol = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                temp += arr[j][i];
+            }
+            maxCol = Math.max(maxCol, temp);
+            temp = 0;
+        }
+
+        int maxDiagonal = 0;
+        for (int i = 0; i < n; i++) {
+            temp += arr[i][i];
+            maxDiagonal = Math.max(maxDiagonal, temp);
+        }
+        temp = 0;
 
         for (int i = 0; i < n; i++) {
-            int rank = 1;
-
-            for (int j = 0; j < n; j++) {
-                if (arr[j] > arr[i]) {
-                    rank++;
-                }
-            }
-            answer[i] = rank;
+            temp += arr[i][n - 1 - i];
+            maxDiagonal = Math.max(maxDiagonal, temp);
         }
+
+        int answer = Math.max(maxRow, Math.max(maxCol, maxDiagonal));
 
         return answer;
     }
@@ -26,14 +46,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] array = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[][] map = new int[n][n];
         for (int i = 0; i < n; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        for (int rank : main.solution(n, array)) {
-            System.out.print(rank + " ");
-        }
+        System.out.println(main.solution(n, map));
     }
 }
