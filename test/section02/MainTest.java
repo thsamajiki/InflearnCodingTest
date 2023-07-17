7b3ec0039ea2f7a50395919030756433db34b0b1
thsamajiki <ant1102@naver.com>
2023-07-17 19:24:02 +0900

    6. 뒤집은 소수 인프런 풀이

--- test/section02/MainTest.java
+++ test/section02/MainTest.java
@@ -12,7 +12,7 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(8, main.solution(20));
+        assertEquals(new ArrayList<>(Arrays.asList(23, 2, 73, 2, 3)), main.solution(9, new int[] { 32, 55, 62, 20, 250, 370, 200, 30, 100 }));
     }
 
     @Test
