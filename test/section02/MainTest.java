bc3ea13d707e78fd3241366997c03456ff3c5718
thsamajiki <ant1102@naver.com>
2023-07-18 11:53:24 +0900

    10. 봉우리 인프런 풀이

--- test/section02/MainTest.java
+++ test/section02/MainTest.java
@@ -12,11 +12,11 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(155, main.solution(5, new int[][] { {10, 13, 10, 12, 15 },
-                                                                        {12, 39, 30, 23, 11},
-                                                                        {11, 25, 50, 53, 15},
-                                                                        {19, 27, 29, 37, 27},
-                                                                        {19, 13, 30, 13, 19}
+        assertEquals(10, main.solution(5, new int[][] { { 5, 3, 7, 2, 3 },
+                                                                        { 3, 7, 1, 6, 1 },
+                                                                        { 7, 2, 5, 3, 4 },
+                                                                        { 4, 3, 6, 4, 1 },
+                                                                        { 8, 7, 3, 5, 2 }
                                                                 })
         );
     }
