2d801ae63be2c187e01f29d7f01db2d63594d23c
thsamajiki <ant1102@naver.com>
2023-07-21 03:13:15 +0900

    12. 멘토링 인프런 풀이

--- test/section02/MainTest.java
+++ test/section02/MainTest.java
@@ -12,12 +12,9 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(4, main.solution(5, new int[][] { { 0, 0, 0, 0, 0, 0 },
-                                                                        { 0, 2, 3, 1, 7, 3 },
-                                                                        { 0, 4, 1, 9, 6, 8 },
-                                                                        { 0, 5, 5, 2, 4, 4 },
-                                                                        { 0, 6, 5, 2, 6, 7 },
-                                                                        { 0, 8, 4, 2, 2, 2 }
+        assertEquals(3, main.solution(4, 3, new int[][] { { 3, 4, 1, 2 },
+                                                                        { 4, 3, 2, 1 },
+                                                                        { 3, 1, 4, 2 }
                                                                 })
         );
     }
