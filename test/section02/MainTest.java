744920fe9b2412ac472c513691ed37786add149f
thsamajiki <ant1102@naver.com>
2023-07-20 19:01:30 +0900

    11. 임시반장 정하기 인프런 풀이

--- test/section02/MainTest.java
+++ test/section02/MainTest.java
@@ -12,11 +12,12 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(4, main.solution(5, new int[][] { { 2, 3, 1, 7, 3 },
-                                                                        { 4, 1, 9, 6, 8 },
-                                                                        { 5, 5, 2, 4, 4 },
-                                                                        { 6, 5, 2, 6, 7 },
-                                                                        { 8, 4, 2, 2, 2 }
+        assertEquals(4, main.solution(5, new int[][] { { 0, 0, 0, 0, 0, 0 },
+                                                                        { 0, 2, 3, 1, 7, 3 },
+                                                                        { 0, 4, 1, 9, 6, 8 },
+                                                                        { 0, 5, 5, 2, 4, 4 },
+                                                                        { 0, 6, 5, 2, 6, 7 },
+                                                                        { 0, 8, 4, 2, 2, 2 }
                                                                 })
         );
     }
