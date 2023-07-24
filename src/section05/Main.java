113729c8c5a01b387e545d98d8122672ace9d73e
thsamajiki <ant1102@naver.com>
2023-07-24 16:50:55 +0900

    3. 크레인 인형뽑기(카카오) 나의 풀이

--- src/section05/Main.java
+++ src/section05/Main.java
@@ -4,20 +4,25 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public String solution(String str) {
-        String answer = "";
-        Stack<Character> stack = new Stack<>();
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
 
-        for (char x : str.toCharArray()) {
-            if (x == ')') {
-                while (stack.pop() != '(');  // stack에서 꺼낸 값이 '('이면 while문이 멈춘다.
-            } else {
-                stack.push(x);
+        List<Integer> dollList = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < moves.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][moves[i] - 1] != 0) {
+                    dollList.add(board[j][moves[i] - 1]); // 바구니에 인형을 넣음
+                    board[j][moves[i] - 1] = 0;
+                    break;
+                }
             }
-        }
 
-        for (int i = 0; i < stack.size(); i++) {
-            answer += stack.get(i);
+            if (dollList.size() >= 2 && dollList.get(dollList.size() - 2) == dollList.get(dollList.size() - 1)) {
+                dollList.remove(dollList.get(dollList.size() - 1));
+                dollList.remove(dollList.get(dollList.size() - 1));
+                answer += 2;
+            }
         }
 
         return answer;
@@ -26,8 +31,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-        System.out.println(main.solution(str));
+        int m = Integer.parseInt(br.readLine());
+        int[] moves = new int[m];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            moves[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(main.solution(board, moves));
     }
 }
