87580f044f7fe141fbdc2cdf0f35bc64c482f930
thsamajiki <ant1102@naver.com>
2023-07-25 02:06:05 +0900

    5. 쇠막대기 인프런 풀이

--- src/section05/Main.java
+++ src/section05/Main.java
@@ -7,32 +7,19 @@ public class Main {
     public int solution(String str) {
         int answer = 0;
 
-        // 후위표기식
-        // 왼쪽에서 부터 순차적으로 읽기 시작한다.
-        // 피연산자(숫자)는 일단 지나치고, 연산자(+-*/)가 나오게 되면, 연산자 앞쪽 두 개의 숫자로 연산을 진행한다.
-
-        Stack<Integer> stack = new Stack<>();
-
-        for (char x : str.toCharArray()) {
-            if (Character.isDigit(x)) {
-                stack.push(x - 48);
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                stack.push('(');
             } else {
-                int right = stack.pop();
-                int left = stack.pop();
-
-                if (x == '+') {
-                    stack.push(left + right);
-                } else if (x == '-') {
-                    stack.push(left - right);
-                } else if (x == '*') {
-                    stack.push(left * right);
-                } else if (x == '/') {
-                    stack.push(left / right);
+                stack.pop();
+                if (str.charAt(i - 1) == '(') {
+                    answer += stack.size();
+                } else {
+                    answer++;
                 }
             }
         }
-
-        answer = stack.get(0);
 
         return answer;
     }
