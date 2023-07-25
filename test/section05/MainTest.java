4c41dcb4d0d9d07635837a6929f8ce3e5a0f9b70
thsamajiki <ant1102@naver.com>
2023-07-25 14:59:38 +0900

    6. 공주 구하기 나의 풀이

--- test/section05/MainTest.java
+++ test/section05/MainTest.java
@@ -9,8 +9,12 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(17, main.solution("()(((()())(())()))(())"));
-        assertEquals(24, main.solution("(((()(()()))(())()))(()())"));
+        assertEquals(7, main.solution(8, 3));
+        assertEquals(20, main.solution(20, 3));
+        assertEquals(91, main.solution(100, 3));
+        assertEquals(494, main.solution(500, 8));
+        assertEquals(212, main.solution(700, 9));
+        assertEquals(329, main.solution(1000, 9));
     }
 
     @Test
