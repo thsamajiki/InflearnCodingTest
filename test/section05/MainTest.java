54725d578b9ec27a48df65bd694d9c757c0666bc
thsamajiki <ant1102@naver.com>
2023-07-25 21:13:45 +0900

    7. 교육과정 설계 인프런 풀이

--- test/section05/MainTest.java
+++ test/section05/MainTest.java
@@ -9,12 +9,7 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(7, main.solution(8, 3));
-        assertEquals(20, main.solution(20, 3));
-        assertEquals(91, main.solution(100, 3));
-        assertEquals(494, main.solution(500, 8));
-        assertEquals(212, main.solution(700, 9));
-        assertEquals(329, main.solution(1000, 9));
+        assertEquals("YES", main.solution("CBA", "CBDAGE"));
     }
 
     @Test
