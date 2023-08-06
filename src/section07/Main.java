9a9f8a64f8019ee42f68afa43f8b97309e55a535
thsamajiki <ant1102@naver.com>
2023-08-06 15:42:21 +0900

    6. 부분집합 구하기 인프런 풀이

--- src/section07/Main.java
+++ src/section07/Main.java
@@ -1,44 +1,36 @@
 package section07;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-class Node {
-    int data;
-    Node left, right;
-
-    public Node(int value) {
-        data = value; // 노드 값
-        left = right = null; // 왼쪽/오른쪽 자식 노드의 번짓수
-    }
-}
+import java.io.*;
 
 public class Main {
-    Node root;
+    static int n;
+    static int[] ch;
+    public void DFS(int L) {
+        if (L == n + 1) {
+            String temp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) {
+                    temp += i + " ";
+                }
+            }
 
-    public void DFS(Node root) {
-        if (root == null) { // root가 말단 노드에 있으면 종료
-            return;
+            if (temp.length() > 0) {
+                System.out.println(temp);
+            }
         } else {
-            DFS(root.left);
-            DFS(root.right);
-            System.out.print(root.data + " ");
+            ch[L] = 1;
+            DFS(L + 1);
+            ch[L] = 0;
+            DFS(L + 1);
         }
     }
 
     public static void main(String[] args) throws IOException {
-        Main tree = new Main();
+        Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
-        tree.root.right.left = new Node(6);
-        tree.root.right.right = new Node(7);
-
-        tree.DFS(tree.root);
+        n = Integer.parseInt(br.readLine());
+        ch = new int[n + 1];
+        main.DFS(1);
     }
 }
