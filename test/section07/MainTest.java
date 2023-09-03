144a31fa7bc3e651ce49a0b2a32a9846749fb182
thsamajiki <ant1102@naver.com>
2023-09-03 14:17:20 +0900

    송아지 인프런 풀이

--- test/section07/MainTest.java
+++ test/section07/MainTest.java
@@ -2,12 +2,14 @@ package section07;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-//        assertEquals(3, main.BFS(5, 14));
+        assertEquals(3, main.BFS(5, 14));
     }
 
     @Test
