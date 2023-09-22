971c79d1c59fdd51e89975bcfd243dca4d797cea
thsamajiki <ant1102@naver.com>
2023-09-22 22:17:52 +0900

    8. 응급실 인프런 풀이

--- src/section05/Main.java
+++ src/section05/Main.java
@@ -5,25 +5,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Person {
+    int id;
+    int priority;
+
+    public Person(int id, int priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+}
 
 public class Main {
-    public String solution(String need, String plan) {
-        String answer = "YES";
-        Queue<Character> q = new LinkedList<>();
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Queue<Person> q = new LinkedList<>();
 
-        for (char x : need.toCharArray()) {
-            q.offer(x);
+        for (int i = 0; i < n; i++) {
+            q.offer(new Person(i, arr[i]));
         }
 
-        for (char y : plan.toCharArray()) {
-            if (q.contains(y)) {
-                if (y != q.poll()) {
-                    return "NO";
+        while (!q.isEmpty()) {
+            Person temp = q.poll();
+
+            for (Person x : q) {
+                if (x.priority > temp.priority) {
+                    q.offer(temp); // temp보다 위험도가 높은 환자가 있으면 temp를 q의 뒤로 보낸다.
+                    temp = null;
+                    break;
                 }
             }
-        }
 
-        if (!q.isEmpty()) return "NO";
+            if (temp != null) {
+                answer++;
+                if (temp.id == m) return answer;
+            }
+        }
 
         return answer;
     }
@@ -32,9 +50,17 @@ public class Main {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String n = br.readLine();
-        String p = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        System.out.println(main.solution(n, p));
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(main.solution(n, m, arr));
     }
 }
