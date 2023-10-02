705add44151a80f16d402ced4c251486a18485ac
thsamajiki <ant1102@naver.com>
2023-10-02 14:23:44 +0900

    1. 합이 같은 부분집합 인프런 풀이

---
+++ test/section08/MainTest.java
@@ -0,0 +1,14 @@
+package section08;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void solution() {
+        Main main = new Main();
+//        assertEquals(3, main.BFS(5, 14));
+    }
+}
