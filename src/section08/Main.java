3ae622d25aa6e56f047c658a17ba0bc25a12cca8
thsamajiki <ant1102@naver.com>
2023-10-09 01:55:39 +0900

    5. 동전 교환 인프런 풀이

--- src/section08/Main.java
+++ src/section08/Main.java
@@ -4,34 +4,42 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M;
-    static int[] pm;
+    static int N, M, answer = Integer.MAX_VALUE;
 
-    public void DFS(int L) {
-        if (L == M) {
-            for (int x : pm) {
-                System.out.print(x + " ");
-            }
-            System.out.println();
+    public void DFS(int L, int sum, Integer[] arr) {
+        if (sum > M) {
+            return; // 스택 오버플로우 방지
+        }
+        if (L >= answer) {
+            return;
+        }
+        if (sum == M) {
+            answer = Math.min(answer, L);
         } else {
-            for (int i = 1; i <= N; i++) {
-                pm[L] = i;
-                DFS(L + 1);
+            for (int i = 0; i < N; i++) {
+                DFS(L + 1, sum + arr[i], arr);
             }
         }
-
     }
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        pm = new int[M];
+        N = Integer.parseInt(br.readLine());
+
+        Integer[] arr = new Integer[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        M = Integer.parseInt(br.readLine());
         
-        main.DFS(0);
+        main.DFS(0, 0, arr);
+
+        System.out.println(answer);
     }
 }
