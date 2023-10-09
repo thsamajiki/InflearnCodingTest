a058a40e5a8341679b4fe0845257d2ca3eef1d32
thsamajiki <ant1102@naver.com>
2023-10-09 18:12:22 +0900

    10. 미로 탐색 인프런 풀이

--- src/section08/Main.java
+++ src/section08/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int n, m;
     static int[][] graph;
     static int[] dx = { -1, 0, 1, 0 };
     static int[] dy = { 0, 1, 0, -1 };
@@ -11,13 +12,13 @@ public class Main {
     static int answer;
 
     public void DFS(int x, int y) {
-        if (x == 6 && y == 6) {
+        if (x == 7 && y == 7) {
             answer++;
         } else {
             for (int i = 0; i < 4; i++) {
                 int nextX = x + dx[i];
                 int nextY = y + dy[i];
-                if (nextX >= 0 && nextX < 7 && nextY >= 0 && nextY < 7 && ch[nextX][nextY] == 0 && graph[nextX][nextY] == 0) {
+                if (nextX >= 1 && nextX <= 7 && nextY >= 1 && nextY <= 7 && ch[nextX][nextY] == 0 && graph[nextX][nextY] == 0) {
                     ch[nextX][nextY] = 1;
                     DFS(nextX, nextY);
                     ch[nextX][nextY] = 0;
@@ -30,18 +31,18 @@ public class Main {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        graph = new int[7][7];
-        ch = new int[7][7];
+        graph = new int[8][8];
+        ch = new int[8][8];
 
-        for (int i = 0; i < 7; i++) {
+        for (int i = 1; i <= 7; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 7; j++) {
+            for (int j = 1; j <= 7; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        ch[0][0] = 1;
-        main.DFS(0, 0);
+        ch[1][1] = 1;
+        main.DFS(1, 1);
 
         System.out.println(answer);
     }
