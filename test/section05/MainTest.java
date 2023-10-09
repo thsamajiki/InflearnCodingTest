28adfe6e2690852aa8f28e3fcff0582403191c89
thsamajiki <ant1102@naver.com>
2023-10-09 09:22:42 +0900

    4. 후위식 연산(postfix) 인프런 풀이

--- test/section05/MainTest.java
+++ test/section05/MainTest.java
@@ -9,15 +9,6 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(4, main.solution(new int[][] {
-                    { 0, 0, 0, 0, 0 },
-                    { 0, 0, 1, 0, 3 },
-                    { 0, 2, 5, 0, 1 },
-                    { 4, 2, 4, 4, 2 },
-                    { 3, 5, 1, 3, 1 }
-                },
-                    new int[] { 1, 5, 3, 5, 1, 2, 1, 4 }
-                )
-            );
+        assertEquals(12, main.solution("352+*9-"));
     }
 }
