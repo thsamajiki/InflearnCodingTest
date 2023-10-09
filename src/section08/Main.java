0921e6a17049ee09e41a29389111e69472235580
thsamajiki <ant1102@naver.com>
2023-10-09 18:12:38 +0900

    10. 미로 탐색 인프런 풀이

--- src/section08/Main.java
+++ src/section08/Main.java
@@ -4,7 +4,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m;
     static int[][] graph;
     static int[] dx = { -1, 0, 1, 0 };
     static int[] dy = { 0, 1, 0, -1 };
