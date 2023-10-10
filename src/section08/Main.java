dde19de4b0e64d5aaaac4d2bc0a397311bbdf6a9
thsamajiki <ant1102@naver.com>
2023-10-11 00:34:43 +0900

    13. 섬나라 아일랜드 인프런 풀이

--- src/section08/Main.java
+++ src/section08/Main.java
@@ -4,28 +4,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m;
+    static int n;
     static int[][] board;
-    static int[][] dist;
-    static int[] dx = { -1, 0, 1, 0 };
-    static int[] dy = { 0, 1, 0, -1 };
-    static Queue<int[]> q = new LinkedList<>();
+    static int answer;
+    static int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
+    static int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
-    public void BFS() {
-        while (!q.isEmpty()) {
-            int[] now = q.poll();
-            int nowX = now[0];
-            int nowY = now[1];
+    private void DFS(int x, int y) {
+        for (int i = 0; i < 8; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-
-                if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m && board[nextX][nextY] == 0) {
-                    board[nextX][nextY] = 1;
-                    q.offer(new int[] { nextX, nextY });
-                    dist[nextX][nextY] = dist[nowX][nowY] + 1;
-                }
+            if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < n && board[nextX][nextY] == 1) {
+                board[nextX][nextY] = 0;
+                DFS(nextX, nextY);
             }
         }
     }
@@ -34,64 +26,27 @@ public class Main {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(br.readLine());
 
-        board = new int[n][m];
-        dist = new int[n][m];
+        board = new int[n][n];
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == 1) {
-                    q.offer(new int[] { i, j });
+                    answer++;
+                    board[i][j] = 0;
+                    main.DFS(i, j);
                 }
             }
         }
 
-        boolean isAllRipen = true;
-
-        outer:
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] != 1) {
-                    isAllRipen = false;
-                    break outer;
-                }
-            }
-        }
-
-        if (isAllRipen) {
-            System.out.println(0);
-            return;
-        }
-
-        main.BFS();
-
-        boolean flag = true;
-        int answer = Integer.MIN_VALUE;
-
-        outer:
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 0) {
-                    flag = false;
-                    break outer;
-                }
-            }
-        }
-
-        if (flag) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    answer = Math.max(answer, dist[i][j]);
-                }
-            }
-            System.out.println(answer);
-        } else {
-            System.out.println(-1);
-        }
+        System.out.println(answer);
     }
 }
