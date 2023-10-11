5dabe77537508263d1946d02b12728409510c1dd
thsamajiki <ant1102@naver.com>
2023-10-11 10:01:36 +0900

    3. 크레인 인형뽑기(카카오) 나의 풀이

--- test/section05/MainTest.java
+++ test/section05/MainTest.java
@@ -9,6 +9,15 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals("EFLM", main.solution("(A(BC)D)EF(G(H)(IJ)K)LM(N)"));
+        assertEquals(4, main.solution(new int[][] {
+                    { 0, 0, 0, 0, 0 },
+                    { 0, 0, 1, 0, 3 },
+                    { 0, 2, 5, 0, 1 },
+                    { 4, 2, 4, 4, 2 },
+                    { 3, 5, 1, 3, 1 }
+                },
+                    new int[] { 1, 5, 3, 5, 1, 2, 1, 4 }
+                )
+            );
     }
 }
