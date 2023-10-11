201552f46e7ef11ef67f6a733517ea242e714015
thsamajiki <ant1102@naver.com>
2023-10-12 02:23:05 +0900

    15. 피자배달거리 인프런 풀이

--- src/section08/Main.java
+++ src/section08/Main.java
@@ -4,38 +4,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int s, e;
-    static int answer;
-    static int[] moves = { 1, -1, 5 };
-    static int[] ch;
+    static int n, m, len, answer = Integer.MAX_VALUE;
+    static int[] combi;
+    static List<int[]> hs, pz;
 
-    private void BFS(int s, int e) {
-        ch = new int[10001];
-        ch[s] = 1;
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(s);
-
-        while (!q.isEmpty()) {
-            int len = q.size();
-
-            for (int i = 0; i < len; i++) {
-                int now = q.poll();
-
-                for (int j = 0; j < moves.length; j++) {
-                    int next = now + moves[j];
-
-                    if (next == e) {
-                        answer++;
-                        return;
-                    }
-                    if (next >= 1 && next <= 10000 && ch[next] == 0) {
-                        ch[next] = 1;
-                        q.offer(next);
-                    }
+    public void DFS(int L, int s) {
+        if (L == m) {
+//            for (int x : combi) {
+//                System.out.print(x + " ");
+//            }
+//            System.out.println();
+            int sum = 0;
+            for (int[] h : hs) {
+                int dis = Integer.MAX_VALUE;
+                for (int i : combi) {
+                    dis = Math.min(dis, Math.abs(h[0] - pz.get(i)[0]) + Math.abs(h[1] - pz.get(i)[1]));
                 }
-
+                sum += dis;
             }
-            answer++;
+
+            answer = Math.min(answer, sum);
+        } else {
+            for (int i = s; i < len; i++) {
+                combi[L] = i;
+                DFS(L + 1, i + 1);
+            }
         }
     }
 
@@ -44,10 +37,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        s = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        main.BFS(s, e);
+        hs = new ArrayList<>();
+        pz = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int temp = Integer.parseInt(st.nextToken());
+                if (temp == 1) hs.add(new int[] { i , j });
+                else if (temp == 2) pz.add(new int[] { i , j });
+            }
+        }
+
+        len = pz.size();
+        combi = new int[m];
+
+        main.DFS(0, 0);
 
         System.out.println(answer);
     }
