e8e7b2ffe6ee486663b8057ceda5954d24ac8edc
thsamajiki <ant1102@naver.com>
2023-10-27 17:10:04 +0900

    1. 올바른 괄호 인프런 풀이

--- test/section05/MainTest.java
+++ test/section05/MainTest.java
@@ -9,12 +9,6 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(3, main.solution(5, 2, new int[] { 60, 50, 70, 80, 90 }));
-        assertEquals(4, main.solution(6, 3, new int[] { 70, 60, 90, 60, 60, 60 }));
-    }
-
-    @Test
-    void main() {
-
+        assertEquals("NO", main.solution("(()(()))(()"));
     }
 }
