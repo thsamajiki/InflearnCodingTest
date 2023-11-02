6acdbbc02dd4acea3748c2c3c0c53a55f00e3470
thsamajiki <ant1102@naver.com>
2023-11-02 22:49:02 +0900

    1. 문자 찾기 나의 풀이

--- test/section01/MainTest.java
+++ test/section01/MainTest.java
@@ -1,19 +1,15 @@
 package section01;
 
 import org.junit.jupiter.api.Test;
-import section01.Main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals("COOL", main.solution(4, "#****###**#####**#####**##**" ));
+        assertEquals(2, main.solution("Computercooler", 'c'));
     }
 
     @Test
