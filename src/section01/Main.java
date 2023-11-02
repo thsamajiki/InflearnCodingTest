300a572233f690d6857cb481c576d0164fa8718e
thsamajiki <ant1102@naver.com>
2023-11-02 23:11:04 +0900

    4. 단어 뒤집기 나의 풀이

--- src/section01/Main.java
+++ src/section01/Main.java
@@ -3,31 +3,33 @@ package section01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
-    public String solution(String str) {
-        String answer = "";
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answerList = new ArrayList<>();
 
-        String[] words = str.split(" ");
-        int max = 0;
-
-        for (String word : words) {
-            int length = word.length();
-            if (max < length) {
-                max = length;
-                answer = word;
-            }
+        for (int i = 0; i < str.length; i++) {
+            StringBuilder sb = new StringBuilder(str[i]);
+            answerList.add(sb.reverse().toString());
         }
 
-        return answer;
+        return answerList;
     }
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+        int n = Integer.parseInt(br.readLine());
 
-        System.out.println(main.solution(str));
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = br.readLine();
+        }
+
+        for (String word : main.solution(n, str)) {
+            System.out.println(word);
+        }
     }
 }
