d57209b64d309cccee210059d34605a4feb15e16
thsamajiki <ant1102@naver.com>
2023-11-02 22:54:06 +0900

    2. 대소문자 변환 나의 풀이

--- test/section01/MainTest.java
+++ test/section01/MainTest.java
@@ -9,7 +9,7 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(2, main.solution("Computercooler", 'c'));
+        assertEquals("sTUdy", main.solution("StuDY"));
     }
 
     @Test
