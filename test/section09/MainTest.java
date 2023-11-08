f5d3d43fb6e3202e330ff6a3a8f0b0e6d1b4f4aa
thsamajiki <ant1102@naver.com>
2023-11-08 18:13:19 +0900

    4. 최대수입스케쥴 인프런 풀이

--- test/section09/MainTest.java
+++ test/section09/MainTest.java
@@ -12,11 +12,12 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(2, main.solution(new ArrayList<>(List.of(
-                new Time(14, 's'), new Time(18, 'e'),
-                new Time(12, 's'), new Time(15, 'e'),
-                new Time(15, 's'), new Time(20, 'e'),
-                new Time(20, 's'), new Time(30, 'e'),
-                new Time(5, 's'), new Time(14, 'e')))));
+        assertEquals(150, main.solution(new ArrayList<>(List.of(
+                new Lecture(50, 2),
+                new Lecture(20, 1),
+                new Lecture(40, 2),
+                new Lecture(60, 3),
+                new Lecture(30, 3),
+                new Lecture(30, 1)))));
     }
 }
