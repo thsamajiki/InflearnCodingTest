f2238265be24664c4fa0191da1a20308eaa430ea
thsamajiki <ant1102@naver.com>
2023-11-08 23:08:11 +0900

    1. 두 배열 합치기 인프런 풀이

--- src/section03/Main.java
+++ src/section03/Main.java
@@ -4,22 +4,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
     public ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
         ArrayList<Integer> list = new ArrayList<>();
+        int p1 = 0, p2 = 0;
 
-        for (int i = 0; i < n; i++) {
-            list.add(a[i]);
+        while (p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) {
+                // list.add(a[p1++]);
+                list.add(a[p1]);
+                p1++;
+            } else {
+                // list.add(b[p2++]);
+                list.add(b[p2]);
+                p2++;
+            }
         }
 
-        for (int i = 0; i < m; i++) {
-            list.add(b[i]);
+        while (p1 < n) {
+            // list.add(a[p1++]);
+            list.add(a[p1]);
+            p1++;
         }
 
-        Collections.sort(list);
+        while (p2 < m) {
+            // list.add(b[p2++]);
+            list.add(b[p2]);
+            p2++;
+        }
 
         return list;
     }
