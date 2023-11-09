40a3b137e54250a5b28f34a384a20e13c804488e
thsamajiki <ant1102@naver.com>
2023-11-09 16:48:03 +0900

    3. 최대 매출 인프런 풀이

--- test/section03/MainTest.java
+++ test/section03/MainTest.java
@@ -2,16 +2,13 @@ package section03;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(new ArrayList<>(List.of(2, 3, 5)), main.solution(5, 5, new int[] { 1, 3, 9, 5, 2 }, new int[] { 3, 2, 5, 7, 8 }));
+        assertEquals(56, main.solution(10, 3, new int[] { 12, 15, 11, 20, 25, 10, 20, 19, 13, 15 }));
     }
 }
