e570f8a1a6aa57e1fb345e287193d69eaab7ff70
thsamajiki <ant1102@naver.com>
2023-11-09 16:48:00 +0900

    3. 최대 매출 인프런 풀이

--- src/section03/Main.java
+++ src/section03/Main.java
@@ -3,28 +3,21 @@ package section03;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public ArrayList<Integer> solution(int n, int m, int[] a, int[] b){
-        ArrayList<Integer> answer = new ArrayList<>();
-        int p1 = 0, p2 = 0;
+    public int solution(int n, int k, int[] arr){
+        int answer = 0;
+        int sum = 0;
 
-        Arrays.sort(a);
-        Arrays.sort(b);
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
 
-        while (p1 < n && p2 < m) {
-            if (a[p1] < b[p2]) {
-                p1++;
-            } else if (a[p1] > b[p2]) {
-                p2++;
-            } else {
-                answer.add(a[p1]);
-                p1++;
-                p2++;
-            }
+        answer = sum;
+        for (int i = k; i < n; i++) {
+            sum += arr[i] - arr[i - k];
+            answer = Math.max(answer, sum);
         }
 
         return answer;
@@ -34,25 +27,17 @@ public class Main {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[] a = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
-        }
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int M = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
-        int[] b = new int[M];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            b[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int x : main.solution(N, M, a, b)) {
-            System.out.print(x + " ");
-        }
-        System.out.println();
+        System.out.println(main.solution(N, K, arr));
     }
 }
