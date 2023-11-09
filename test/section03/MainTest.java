9ec7a6fb926d0fb183106be78cfdee0d38796f6a
thsamajiki <ant1102@naver.com>
2023-11-09 11:57:28 +0900

    2. 공통원소 구하기 나의 풀이

--- test/section03/MainTest.java
+++ test/section03/MainTest.java
@@ -12,6 +12,6 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(new ArrayList<>(List.of(1, 2, 3, 3, 5, 6, 7, 9)), main.solution(3, 5, new int[] { 1, 3, 5 }, new int[] { 2, 3, 6, 7, 9 }));
+        assertEquals(new ArrayList<>(List.of(2, 3, 5)), main.solution(5, 5, new int[] { 1, 3, 9, 5, 2 }, new int[] { 3, 2, 5, 7, 8 }));
     }
 }
