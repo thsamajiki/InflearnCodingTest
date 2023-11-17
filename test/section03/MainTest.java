4e0028f533b667462ef23fd457e4eee2554010b2
thsamajiki <ant1102@naver.com>
2023-11-18 00:00:00 +0900

    5. 연속된 자연수의 합(two pointers) 인프런 풀이

--- test/section03/MainTest.java
+++ test/section03/MainTest.java
@@ -9,6 +9,6 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(3, main.solution(8, 6, new int[] { 1, 2, 1, 3, 1, 1, 1, 2 }));
+        assertEquals(3, main.solution(15));
     }
 }
