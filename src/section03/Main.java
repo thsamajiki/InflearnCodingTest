6eb311d5186ef233d6eccdeac7f7c1a5c4b7e1a1
thsamajiki <ant1102@naver.com>
2023-11-18 02:52:55 +0900

    5. 연속된 자연수의 합(수학) 인프런 풀이

--- src/section03/Main.java
+++ src/section03/Main.java
@@ -7,23 +7,15 @@ import java.io.InputStreamReader;
 public class Main {
     public int solution(int n) {
         int answer = 0;
-        int sum = 0;
-        int m = n / 2 + 1; // 예를 들면 n = 15일 때, 연속된 자연수로 8과 9가 나올 수는 없으므로 n / 2 + 1까지만 연속된 자연수가 있으면 된다.
-        int p1 = 1;
+        int count = 1;
 
-        int[] arr = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            arr[i] = i;
-        }
+        n--;
 
-        for (int p2 = 1; p2 <= m; p2++) {
-            sum += arr[p2];
-            if (sum == n) answer++;
+        while (n > 0) {
+            count++;
+            n = n - count;
 
-            while (sum >= n) {
-                sum -= arr[p1++];
-                if (sum == n) answer++;
-            }
+            if (n % count == 0) answer++;
         }
         
         return answer;
