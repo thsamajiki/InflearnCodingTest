383260b52a4ea7a9c9854aaa8a59ab6bb60a45ba
thsamajiki <ant1102@naver.com>
2023-11-19 18:21:29 +0900

    4. 모든 아나그램 찾기 인프런 풀이

--- src/section04/Main.java
+++ src/section04/Main.java
@@ -6,49 +6,24 @@ import java.io.*;
 public class Main {
     public int solution(String a, String b) {
         int answer = 0;
-        Map<Character, Integer> aMap = new HashMap<>();
-        Map<Character, Integer> bMap = new HashMap<>();
+        HashMap<Character, Integer> am = new HashMap<>();
+        HashMap<Character, Integer> bm = new HashMap<>();
+        for (char x : b.toCharArray()) bm.put(x, bm.getOrDefault(x, 0) + 1);
 
-        for (char x : b.toCharArray()) {
-            bMap.put(x, bMap.getOrDefault(x, 0) + 1);
+        int L = b.length() - 1;
+        for (int i = 0; i < L; i++) am.put(a.charAt(i), am.getOrDefault(a.charAt(i), 0) + 1);
+
+        int lt = 0;
+        for (int rt = L; rt < a.length(); rt++) {
+            am.put(a.charAt(rt), am.getOrDefault(a.charAt(rt), 0) + 1);
+
+            if (am.equals(bm)) answer++;
+
+            am.put(a.charAt(lt), am.get(a.charAt(lt)) - 1);
+            if (am.get(a.charAt(lt)) == 0) am.remove(a.charAt(lt));
+
+            lt++;
         }
-        int lengthB = b.length();
-
-        for (int i = 0; i < lengthB; i++) {
-            aMap.put(a.charAt(i), aMap.getOrDefault(a.charAt(i), 0) + 1);
-        }
-        System.out.println("초기");
-        System.out.println("aMap : " + aMap);
-        System.out.println("bMap : " + bMap);
-        System.out.println("================================");
-
-        if (aMap.equals(bMap)) answer++;
-
-        int lengthA = a.length();
-        int left = 0;
-        for (int right = lengthB; right < lengthA; right++) {
-            System.out.println("right : " + right);
-            aMap.put(a.charAt(right), aMap.getOrDefault(a.charAt(right), 0) + 1);
-
-            System.out.println("aMap : " + aMap);
-            System.out.println("aMap.get(a.charAt(left)) : " + aMap.get(a.charAt(left)));
-
-            if (aMap.get(a.charAt(left)) > 1) {
-                System.out.println("경우 1");
-                aMap.put(a.charAt(left), aMap.get(a.charAt(left)) - 1);
-            } else {
-                System.out.println("경우 2");
-                aMap.remove(a.charAt(left));
-            }
-
-            left++;
-            if (aMap.equals(bMap)) answer++;
-            System.out.println("left : " + left);
-            System.out.println("bMap : " + bMap);
-            System.out.println("answer : " + answer);
-            System.out.println("================================\n");
-        }
-
         return answer;
     }
 
@@ -56,9 +31,9 @@ public class Main {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String S = br.readLine();
-        String T = br.readLine();
+        String a = br.readLine();
+        String b = br.readLine();
 
-        System.out.println(main.solution(S, T));
+        System.out.println(main.solution(a, b));
     }
 }
