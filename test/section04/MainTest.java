ef14ec2d00217998ab5c7ae2d9563792f90e5291
thsamajiki <ant1102@naver.com>
2023-11-19 17:29:05 +0900

    4. 모든 아나그램 찾기 남의 풀이

--- test/section04/MainTest.java
+++ test/section04/MainTest.java
@@ -2,9 +2,6 @@ package section04;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
@@ -12,6 +9,6 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(new ArrayList<>(List.of(3, 4, 4, 3)), main.solution(7, 4, new int[] { 20, 12, 20, 10, 23, 17, 10 }));
+        assertEquals(3, main.solution("bacaAacba", "abc"));
     }
 }
