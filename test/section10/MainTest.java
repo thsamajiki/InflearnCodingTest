2d8a77169e2513208c6c566a1a17c71061c32ac7
thsamajiki <ant1102@naver.com>
2023-12-11 01:31:25 +0900

    1. 계단오르기 인프런 풀이

---
+++ test/section10/MainTest.java
@@ -0,0 +1,14 @@
+package section10;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void solution() {
+        Main main = new Main();
+        assertEquals(21, main.solution(7));
+    }
+}
