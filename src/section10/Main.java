a519e242c77804d2f77c55ad213838234c417ab4
thsamajiki <ant1102@naver.com>
2023-12-11 01:58:13 +0900

    2. 돌다리 건너기 인프런 풀이

--- src/section10/Main.java
+++ src/section10/Main.java
@@ -10,11 +10,11 @@ public class Main {
         dy[1] = 1;
         dy[2] = 2;
 
-        for (int i = 3; i <= n; i++) {
+        for (int i = 3; i <= n + 1; i++) {
             dy[i] = dy[i - 2] + dy[i - 1];
         }
 
-        return dy[n];
+        return dy[n + 1]; // n + 1번째 위치에 도착해야 건넌 것임 (n번째 돌에 있으면 건넌게 아님)
     }
 
     public static void main(String[] args) throws IOException {
@@ -22,7 +22,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        dy = new int[n + 1];
+        dy = new int[n + 2];
 
         System.out.println(main.solution(n));
     }
