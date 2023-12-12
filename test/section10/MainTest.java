fb544206416b6f7c30898244fadf3a5b27b11280
thsamajiki <ant1102@naver.com>
2023-12-12 23:59:07 +0900

    6. 최대점수 구하기(냅색 알고리즘) 인프런 풀이

--- test/section10/MainTest.java
+++ test/section10/MainTest.java
@@ -2,8 +2,6 @@ package section10;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
@@ -11,10 +9,10 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        Main.m = 15;
-        Main.n = 3;
+        Main.m = 20;
+        Main.n = 5;
         Main.dy = new int[Main.m + 1];
-        Arrays.fill(Main.dy, Integer.MAX_VALUE);
-        assertEquals(3, main.solution(new int[] { 1, 2, 5 }));
+
+        assertEquals(41, main.solution(new int[] { 10, 25, 15, 6, 7 }, new int[] { 5, 12, 8, 3, 4 }));
     }
 }
