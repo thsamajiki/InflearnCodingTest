b7f557062e59d43f59c5388618e8fa5d04e26ad2
thsamajiki <ant1102@naver.com>
2023-12-12 22:47:32 +0900

    5. 동전교환 인프런 풀이

--- test/section10/MainTest.java
+++ test/section10/MainTest.java
@@ -2,8 +2,7 @@ package section10;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,13 +11,10 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        int n = 5;
-        Main.dy = new int[n];
-        assertEquals(10, main.solution(new ArrayList<>(List.of(
-                new Brick(25, 3, 4),
-                new Brick(4, 4, 6),
-                new Brick(9, 2, 3),
-                new Brick(16, 2, 5),
-                new Brick(1, 5, 2)))));
+        Main.m = 15;
+        Main.n = 3;
+        Main.dy = new int[Main.m + 1];
+        Arrays.fill(Main.dy, Integer.MAX_VALUE);
+        assertEquals(3, main.solution(new int[] { 1, 2, 5 }));
     }
 }
