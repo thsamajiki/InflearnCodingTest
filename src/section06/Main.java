57b326197cf7ff16f5dee373f352d96756cfdec1
thsamajiki <ant1102@naver.com>
2023-12-13 19:54:34 +0900

    2. 버블 정렬 인프런 풀이

--- src/section06/Main.java
+++ src/section06/Main.java
@@ -8,15 +8,13 @@ import java.util.StringTokenizer;
 public class Main {
     public int[] solution(int n, int[] arr) {
         for (int i = 0; i < n - 1; i++) {
-            int idx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[idx] > arr[j]) {
-                    idx = j;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-            int temp = arr[i];
-            arr[i] = arr[idx];
-            arr[idx] = temp;
         }
 
         return arr;
