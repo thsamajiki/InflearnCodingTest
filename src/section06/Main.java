4efee9a4f4a403f2045ce74c7c4e4a660363fa59
thsamajiki <ant1102@naver.com>
2023-12-14 19:53:19 +0900

    5. 중복 확인 나의 풀이

--- src/section06/Main.java
+++ src/section06/Main.java
@@ -3,50 +3,37 @@ package section06;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public int[] solution(int size, int n, int[] arr) {
-        int[] cache = new int[size];
+    public String solution(int n, int[] arr) {
+        String answer = "U";
 
-        for (int x: arr) {
-            int position = -1;
-            for (int i = 0; i < size; i++) { // Cache Hit (이미 해당 원소(x)가 캐시 메모리에 있으면)
-                if (x == cache[i]) position = i;
-            }
-            if (position == -1) { // Cache Miss 해당 원소(x)가 캐시 메모리에 없으면
-                for (int i = size - 1; i >= 1; i--) {
-                    cache[i] = cache[i - 1];
-                }
-                cache[0] = x;
-            } else {
-                for (int i = position; i >= 1; i--) { // Cache Hit (이미 해당 원소(x)가 캐시 메모리에 있으면)
-                    cache[i] = cache[i - 1];
-                }
-                cache[0] = x;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                answer = "D";
+                return answer;
             }
         }
 
-        return cache;
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int x : main.solution(s, n, arr)) {
-            System.out.print(x + " ");
-        }
-        System.out.println();
+        System.out.println(main.solution(n, arr));
     }
 }
