3ebc38c760b4b0315e510c0f0955a039f9c1aa90
thsamajiki <ant1102@naver.com>
2023-12-20 00:39:17 +0900

    6. 친구인가 인프런 풀이

--- src/section09/Main.java
+++ src/section09/Main.java
@@ -3,50 +3,23 @@ package section09;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Edge implements Comparable<Edge> {
-    int vex;
-    int cost;
-
-    public Edge(int vex, int cost) {
-        this.vex = vex;
-        this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Edge o) {
-        return this.cost - o.cost;
-    }
-}
-
 public class Main {
-    static int n, m;
-    static ArrayList<ArrayList<Edge>> graph;
-    static int[] dis;
+    static int[] unf; // 숫자쌍을 담을 배열
 
-    public void solution(int v) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(v, 0));
+    public static void union(int a, int b) {
+        // 두 친구가 같은 무리가 아닌 경우,
+        // 한 무리의 가장 끝에 위치한 친구가 다른 무리의 가장 끝에 위치한 친구를 바라보도록 하여
+        // 두 무리를 하나로 합친다.
+        int fa = find(a);
+        int fb = find(b);
+        if (fa != fb) unf[fa] = fb; // 경로 압축하여 시간 복잡도 감소
+    }
 
-        dis[v] = 0;
-        while (!pq.isEmpty()) {
-            Edge temp = pq.poll();
-            int now = temp.vex;
-            int nowCost = temp.cost;
-
-            if (nowCost > dis[now]) continue;
-
-            for (Edge ob : graph.get(now)) {
-                if (dis[ob.vex] > nowCost + ob.cost) {
-                    dis[ob.vex] = nowCost + ob.cost;
-                    pq.offer(new Edge(ob.vex, dis[ob.vex]));
-                }
-            }
-        }
+    public static int find(int v) { // 해당 학생이 포함된 친구 관계(무리)를 확인
+        if (v == unf[v]) return v;
+        else return unf[v] = find(unf[v]);
     }
 
     public static void main(String[] args) throws IOException {
@@ -54,30 +27,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken()); // 반 학생수
+        int m = Integer.parseInt(st.nextToken()); // 숫자쌍의 개수
 
-        graph = new ArrayList<>();
-        for(int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+        unf = new int[n + 1]; // 인덱스가 학생 번호를 의미, 값이 집합 번호를 의미
+        for (int i = 1; i <= n; i++) {
+            unf[i] = i; // 아직 어떤 집합에 속해있는지 알지 못하기 때문에 인덱스 별로 초기화 해준다.
         }
 
-        dis = new int[n + 1];
-        Arrays.fill(dis, Integer.MAX_VALUE);
-        for (int i = 0; i < m; i++) {
+        for (int i = 1; i <= m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            graph.get(a).add(new Edge(b, c));
+            union(a, b);
         }
 
-        main.solution(1);
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int fa = find(a);
+        int fb = find(b);
 
-        for (int i = 2; i <= n; i++) {
-            if (dis[i] != Integer.MAX_VALUE) System.out.println(i + " : " + dis[i]);
-            else System.out.println(i + " : impossible");
-        }
+        if (fa == fb) System.out.println("YES");
+        else System.out.println("NO");
     }
 }
