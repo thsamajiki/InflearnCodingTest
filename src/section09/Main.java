5680801c9b1e93be5abb82ab98e7f6bdccf06182
thsamajiki <ant1102@naver.com>
2023-12-20 01:36:01 +0900

    7. 원더랜드(최소 스패닝 트리 - 크루스칼 : Union&Find 이용) 인프런 풀이

--- src/section09/Main.java
+++ src/section09/Main.java
@@ -3,23 +3,42 @@ package section09;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int[] unf; // 숫자쌍을 담을 배열
+class Edge implements Comparable<Edge> {
+    int v1;
+    int v2;
+    int cost;
 
-    public static int find(int v) { // 해당 학생이 포함된 친구 관계(무리)를 확인
-        if (v == unf[v]) return v;
-        else return unf[v] = find(unf[v]);
+    public Edge(int v1, int v2, int cost) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return this.cost - o.cost;
+    }
+}
+
+public class Main {
+    static int[] unf;
+
+    public static int find(int v) {
+        if (v == unf[v]) {
+            return v;
+        } else {
+            return unf[v] = find(unf[v]);
+        }
     }
 
     public static void union(int a, int b) {
-        // 두 친구가 같은 무리가 아닌 경우,
-        // 한 무리의 가장 끝에 위치한 친구가 다른 무리의 가장 끝에 위치한 친구를 바라보도록 하여
-        // 두 무리를 하나로 합친다.
         int fa = find(a);
         int fb = find(b);
-        if (fa != fb) unf[fa] = fb; // 경로 압축하여 시간 복잡도 감소
+        if (fa != fb) unf[fa] = fb;
     }
 
     public static void main(String[] args) throws IOException {
@@ -27,28 +46,37 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); // 반 학생수
-        int m = Integer.parseInt(st.nextToken()); // 숫자쌍의 개수
+        int v = Integer.parseInt(st.nextToken()); // 도시의 개수
+        int e = Integer.parseInt(st.nextToken()); // 도로의 개수
 
-        unf = new int[n + 1]; // 인덱스가 학생 번호를 의미, 값이 집합 번호를 의미
-        for (int i = 1; i <= n; i++) {
+        unf = new int[v + 1]; // 인덱스가 도시 번호를 의미, 값이 집합 번호를 의미
+        ArrayList<Edge> list = new ArrayList<>();
+        for (int i = 1; i <= v; i++) {
             unf[i] = i; // 아직 어떤 집합에 속해있는지 알지 못하기 때문에 인덱스 별로 초기화 해준다.
         }
 
-        for (int i = 1; i <= m; i++) {
+        for (int i = 1; i <= e; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            union(a, b);
+            int c = Integer.parseInt(st.nextToken());
+            list.add(new Edge(a, b, c));
         }
 
-        st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int fa = find(a);
-        int fb = find(b);
+        int answer = 0;
 
-        if (fa == fb) System.out.println("YES");
-        else System.out.println("NO");
+        Collections.sort(list);
+
+        for(Edge edge: list) {
+            int fv1 = find(edge.v1);
+            int fv2 = find(edge.v2);
+
+            if (fv1 != fv2) {
+                answer += edge.cost;
+                union(edge.v1, edge.v2);
+            }
+        }
+
+        System.out.println(answer);
     }
 }
