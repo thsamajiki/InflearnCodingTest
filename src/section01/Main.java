4b62d716f20689de76bcb7a0f7aa0f808ab4a3d5
thsamajiki <ant1102@naver.com>
2024-01-06 03:03:42 +0900

    10. 가장 짧은 문자거리 인프런 풀이

--- src/section01/Main.java
+++ src/section01/Main.java
@@ -3,24 +3,49 @@ package section01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public int solution(String s) {
-        String answer = "";
+    public int[] solution(String s, char t) {
+        int[] answer = new int[s.length()];
 
-        for(char x: s.toCharArray()) {
-            if (Character.isDigit(x)) answer += x;
+        int p = 1000;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == t) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
         }
 
-        return Integer.parseInt(answer);
+        p = 1000;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == t) {
+                p = 0;
+            } else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+        char ch = st.nextToken().charAt(0);
 
-        System.out.println(main.solution(str));
+        for(int x: main.solution(str, ch)) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
     }
 }
