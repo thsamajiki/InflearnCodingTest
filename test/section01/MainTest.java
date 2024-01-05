c1e78fb4976a513c6efac6c18e94e478978df14e
thsamajiki <ant1102@naver.com>
2024-01-06 03:15:57 +0900

    11. 문자열 압축 인프런 풀이

--- test/section01/MainTest.java
+++ test/section01/MainTest.java
@@ -2,13 +2,14 @@ package section01;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-        assertArrayEquals(new int[] { 1, 0, 1, 2, 1, 0, 1, 2, 2, 1, 0 }, main.solution("teachermode", 'e'));
+        assertEquals("K2HS7E", main.solution("KKHSSSSSSSE"));
+        assertEquals("KST3SE2KFK3DJ2G2", main.solution("KSTTTSEEKFKKKDJJGG"));
     }
 }
