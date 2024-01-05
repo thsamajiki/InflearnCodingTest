8066d3e9cdfe0d468b5ebbc37af10795cb2b141d
thsamajiki <ant1102@naver.com>
2024-01-06 02:42:09 +0900

    8. 유효한 팰린드롬 인프런 풀이

--- src/section01/Main.java
+++ src/section01/Main.java
@@ -3,27 +3,16 @@ package section01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
-    public ArrayList<String> solution(int n, String[] str) {
-        ArrayList<String> answer = new ArrayList<>();
-        
-        for(String word: str) {
-            char[] s = word.toCharArray();
-            int left = 0;
-            int right = s.length - 1;
+    public String solution(String s) {
+        String answer = "NO";
 
-            while(left < right) {
-                char temp = s[left];
-                s[left] = s[right];
-                s[right] = temp;
-                left++;
-                right--;
-            }
+        s = s.toUpperCase().replaceAll("[^A-Z]", "");
 
-            answer.add(String.valueOf(s));
-        }
+        String temp = new StringBuilder(s).reverse().toString();
+
+        if (s.equals(temp)) answer = "YES";
 
         return answer;
     }
@@ -32,15 +21,8 @@ public class Main {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
 
-        String[] str = new String[n];
-        for (int i = 0; i < n; i++) {
-            str[i] = br.readLine();
-        }
-
-        for (String word : main.solution(n, str)) {
-            System.out.println(word);
-        }
+        System.out.println(main.solution(str));
     }
 }
