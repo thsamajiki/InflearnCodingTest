57fb7946f866f9e54e693023d3930567643fd453
thsamajiki <ant1102@naver.com>
2024-01-06 12:13:50 +0900

    5. 소수(에라토스테네스 체) 인프런 풀이

--- src/section02/Main.java
+++ src/section02/Main.java
@@ -1,33 +1,19 @@
 package section02;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public int solution(int n, int m, int[][] arr) {
+    public int solution(int n) {
         int answer = 0;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                int count = 0;
-                for (int k = 0; k < m; k++) { // i번 학생의 가능한 등수
-                    int positionI = 0;
-                    int positionJ = 0;
-                    for (int s = 0; s < n; s++) { // j번 학생의 가능한 등수
-                        if (arr[k][s] == i) {
-                            positionI = s; // 이 때 i번 학생의 등수
-                        }
-                        if (arr[k][s] == j) {
-                            positionJ = s; // 이 때 j번 학생의 등수
-                        }
-                    }
-
-                    if (positionI < positionJ) {
-                        count++;
-                    }
-                }
-                if (count == m) {
-                    answer++;
+        int[] ch = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            if (ch[i] == 0) {
+                answer++;
+                for (int j = i; j <= n; j += i) {
+                    ch[j] = 1;
                 }
             }
         }
@@ -38,18 +24,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        int n = Integer.parseInt(br.readLine());
 
-        System.out.println(main.solution(n, m, arr));
+        System.out.println(main.solution(n));
     }
 }
