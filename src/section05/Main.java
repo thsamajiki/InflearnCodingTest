a2ab6e9d34b232c47e7aa93d0e4f0d82dfd011f0
thsamajiki <ant1102@naver.com>
2024-01-06 14:41:26 +0900

    1. 올바른 괄호 인프런 풀이

--- src/section05/Main.java
+++ src/section05/Main.java
@@ -3,45 +3,22 @@ package section05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-class Person {
-    int id;
-    int priority;
-
-    public Person(int id, int priority) {
-        this.id = id;
-        this.priority = priority;
-    }
-}
+import java.util.Stack;
 
 public class Main {
-    public int solution(int n, int m, int[] arr) {
-        int answer = 0;
-        Queue<Person> q = new LinkedList<>();
+    public String solution(String str) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < n; i++) {
-            q.offer(new Person(i, arr[i]));
-        }
-
-        while (!q.isEmpty()) {
-            Person temp = q.poll();
-
-            for (Person x : q) {
-                if (x.priority > temp.priority) {
-                    q.offer(temp); // temp보다 위험도가 높은 환자가 있으면 temp를 q의 뒤로 보낸다.
-                    temp = null;
-                    break;
-                }
-            }
-
-            if (temp != null) {
-                answer++;
-                if (temp.id == m) return answer;
+        for (char x: str.toCharArray()) {
+            if (x == '(') stack.push(x);
+            else {
+                if (stack.isEmpty()) return "NO";
+                else stack.pop();
             }
         }
+
+        if (!stack.isEmpty()) answer = "NO";
 
         return answer;
     }
@@ -50,17 +27,8 @@ public class Main {
         Main main = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        String str = br.readLine();
 
-        int[] arr = new int[n];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        System.out.println(main.solution(n, m, arr));
+        System.out.println(main.solution(str));
     }
 }
