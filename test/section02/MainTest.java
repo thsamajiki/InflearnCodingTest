3d9dd61f88172b5ceb396e5411a006ce14419459
thsamajiki <ant1102@naver.com>
2024-01-06 12:13:57 +0900

    5. 소수(에라토스테네스 체) 인프런 풀이

--- test/section02/MainTest.java
+++ test/section02/MainTest.java
@@ -2,24 +2,13 @@ package section02;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void solution() {
         Main main = new Main();
-        assertEquals(3, main.solution(4, 3, new int[][] { { 3, 4, 1, 2 },
-                                                                        { 4, 3, 2, 1 },
-                                                                        { 3, 1, 4, 2 }
-                                                                })
-        );
-    }
-
-    @Test
-    void main() {
+        assertEquals(8, main.solution(20));
     }
 }
