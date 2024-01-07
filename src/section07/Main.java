97179ef458c2b7eac6f24e53d4bc617a08bd12ac
thsamajiki <ant1102@naver.com>
2024-01-07 13:09:13 +0900

    13. 경로 탐색(인접 리스트) 인프런 풀이

--- src/section07/Main.java
+++ src/section07/Main.java
@@ -3,21 +3,22 @@ package section07;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m, answer;
-    static int[][] edges;
+    static ArrayList<ArrayList<Integer>> edges;
     static boolean[] visited;
 
     public void DFS(int v) {
         if (v == n) answer++;
         else {
-            for (int i = 1; i <= n; i++) {
-                if (edges[v][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    DFS(i);
-                    visited[i] = false;
+            for (int nextV: edges.get(v)) {
+                if (!visited[nextV]) {
+                    visited[nextV] = true;
+                    DFS(nextV);
+                    visited[nextV] = false;
                 }
             }
         }
@@ -31,13 +32,18 @@ public class Main {
         n = Integer.parseInt(st.nextToken()); // 정점의 수
         m = Integer.parseInt(st.nextToken()); // 간선의 수
 
-        edges = new int[n + 1][n + 1];
+        edges = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            edges.add(new ArrayList<>());
+        }
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
 
-            edges[from][to] = 1;
+            edges.get(from).add(to);
         }
 
         visited = new boolean[n + 1];
