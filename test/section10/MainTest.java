f766d3a29e3465c477bd6f9d4653162c57043092
thsamajiki <ant1102@naver.com>
2024-02-02 18:38:21 +0900

    3. 최대 부분 증가수열 인프런 풀이

--- test/section10/MainTest.java
+++ test/section10/MainTest.java
@@ -9,10 +9,7 @@ class MainTest {
     @Test
     void solution() {
         Main main = new Main();
-        Main.m = 20;
-        Main.n = 5;
-        Main.dy = new int[Main.m + 1];
 
-        assertEquals(41, main.solution(new int[] { 10, 25, 15, 6, 7 }, new int[] { 5, 12, 8, 3, 4 }));
+        assertEquals(4, main.solution(new int[] { 5, 3, 7, 8, 6, 2, 9, 4 }));
     }
 }
